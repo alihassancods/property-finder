3feb6b585edb01105891f16f49e4867315e43c97
Ali hassan <alihassan2023@protonmail.com>
2024-12-25 17:53:39 +0500

    Changed settings.py for deployment test

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i%n#wnuykf2m6%-a-l&lkgicx3cjs)uwj&23-2iped@jesmjvy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://alihassancodes.pythonanywhere.com/']
 
 
 # Application definition
